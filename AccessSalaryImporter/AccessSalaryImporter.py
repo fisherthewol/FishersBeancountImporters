c287520b8a9b9c682259b4116a81dda57ac0a9eb
George Kokinis <george.kokinis@gmail.com>
2023-01-07 01:40:03 +0000

    Check for mimetype.

--- AccessSalaryImporter/AccessSalaryImporter.py
+++ AccessSalaryImporter/AccessSalaryImporter.py
@@ -1,12 +1,15 @@
-from beancount.ingest import importer
+from beancount.ingest import importer, cache
 
 
 class AccessSalaryImporter(importer.ImporterProtocol):
+    """A Beancount importer for Access UK Payslips."""
 
     def __init__(self, incomeaccount: str, checkingaccount: str):
         self.incomeAccount = incomeaccount
         self.checkingAccount = checkingaccount
         self.currency = "GBP"
 
-    def identify(self, file):
+    def identify(self, file: cache._FileMemo):
+        if file.mimetype() != 'application/pdf':
+            return False
         raise NotImplementedError("Not Implemented")
