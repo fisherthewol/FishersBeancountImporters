37e5ee9750ccff10c4f77c9826bd9bee053a1f39
George Kokinis <george.kokinis@gmail.com>
2023-01-07 02:30:22 +0000

    Extract text and test for Access.

--- AccessSalaryImporter/AccessSalaryImporter.py
+++ AccessSalaryImporter/AccessSalaryImporter.py
@@ -1,4 +1,15 @@
+from PyPDF2 import PdfReader
 from beancount.ingest import importer, cache
+import re
+
+
+def pdf_to_text(filename: str):
+    """Convert pdf file to text."""
+    reader = PdfReader(filename)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
 
 
 class AccessSalaryImporter(importer.ImporterProtocol):
@@ -8,8 +19,14 @@ class AccessSalaryImporter(importer.ImporterProtocol):
         self.incomeAccount = incomeaccount
         self.checkingAccount = checkingaccount
         self.currency = "GBP"
+        self.cachedPDF = None
 
-    def identify(self, file: cache._FileMemo):
+    def identify(self, file: cache._FileMemo) -> bool:
         if file.mimetype() != 'application/pdf':
             return False
-        raise NotImplementedError("Not Implemented")
+
+        self.cachedPDF = file.convert(pdf_to_text)
+        if self.cachedPDF:
+            return re.match('ACCESS UK', self.cachedPDF) is not None
+
+        return False
