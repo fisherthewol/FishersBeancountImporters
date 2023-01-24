f600b874e12c258d2bd3eef0efa860c8fbcaf281
George Kokinis <george.kokinis@gmail.com>
2023-01-24 22:40:04 +0000

    Fix name to get setup working.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name="Fisher'sBeanCountImporters",
+    name="FishersBeanCountImporters",
     version="0.0.1",
     author="fisherthewol",
     py_modules=['AccessSalaryImporter']
