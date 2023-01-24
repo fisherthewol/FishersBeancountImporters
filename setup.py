71c189c39ae2ab5f33a2284dd9e3d769e37cba9e
George Kokinis <george.kokinis@gmail.com>
2023-01-24 22:49:44 +0000

    Add explicit dependencies.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name="FishersBeanCountImporters",
     version="0.0.1",
     author="fisherthewol",
-    py_modules=['AccessSalaryImporter']
+    packages=find_packages(['AccessSalaryImporter']),
+    install_requires=[
+        'PyPDF2>=3.0.1',
+        'beancount @ git+https://github.com/beancount/beancount.git@6d64d56b2e04dd164cd117c124d703da89783ee1',
+    ]
 )
