9de3ee479f50c42998cf6f6425fc1ab766c30719
George Kokinis <george.kokinis@gmail.com>
2023-01-25 20:32:00 +0000

    Add test code for Importer.

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ def main():
     from beancount.ingest import cache
     import os
 
-    fc = cache.get_file(os.getcwd() + "/AccessSalaryImporter/My Payslip 22-DEC-22.pdf")
-    imp = AccessSalaryImporter.Importer("", "", "20")
-    print(imp.identify(fc))
-    x = imp.extract(fc)
+    # fc = cache.get_file(os.getcwd() + "/AccessSalaryImporter/My Payslip 22-DEC-22.pdf")
+    # imp = AccessSalaryImporter.Importer("", "", "20")
+    # print(imp.identify(fc))
+    # x = imp.extract(fc)
+    # print(x)
+    import FirstAccountImporter
+    from beancount.ingest import cache
+    fc = cache.get_file(os.getcwd() + "/FirstAccountImporter/25012023_1621.csv")
+    imp = FirstAccountImporter.Importer("")
+    x = imp.identify(fc)
     print(x)
 
 
