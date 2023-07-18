730283a917b8aa6046541edd2fa5262171930765
George Kokinis <george.kokinis@gmail.com>
2023-07-18 11:24:13 +0100

    Update to allow keys to be passed; allow specifying whether to invert value.

--- Heuristics/Heuristics.py
+++ Heuristics/Heuristics.py
@@ -1,21 +1,29 @@
 from typing import Dict
 
+from beancount.core import flags
 from beancount.core.amount import Amount
 from beancount.core.data import Posting
 from beancount.core.number import D
 
 
 class Heuristics:
-    def __init__(self):
+    def __init__(self, payeecolumn: str, valuecolumn: str, currency: str, invertvalue: bool = True):
         self.groceryStores = ['tesco', 'morrison', 'lidl', 'aldi']
+        self.payeeColumn = payeecolumn
+        self.valueColumn = valuecolumn
+        self.currency = currency
+        self.invertValue = invertvalue
 
     def identify_groceries(self, row: Dict[str], groceriesaccount: str) -> Posting:
-        desc: str = row['Description']
+        desc: str = row[self.payeeColumn]
         for store in self.groceryStores:
             if store in desc.lower():
                 return Posting(
                     account=groceriesaccount,
-                    units=Amount(-D(row['Amount']), self.currency),
-                    cost=None, price=None, flag=None, meta=None
+                    units=Amount(
+                        -D(row[self.valueColumn]) if self.invertValue else D(row[self.valueColumn]),
+                        self.currency
+                    ),
+                    cost=None, price=None, flag=flags.FLAG_WARNING, meta=None
                 )
         return None
