45c8a9e119c045a715c8a54d671771b29bb3bffd
George Kokinis <george.kokinis@gmail.com>
2024-05-13 22:44:09 +0100

    Update package __init__.py.

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from AccessSalaryImporter import Importer as AccessSalaryImporter
 from FirstAccountImporter import Importer as FirstAccountImporter
 from AmexCSVImporter import Importer as AmexCSVImporter
 from HSBCCCImporter import Importer as HSBCCCImporter
+from QifImporter import QifImporter
