e081bb0a2f58d74353a9658f473756f45bff6f2c
George Kokinis <george.kokinis@gmail.com>
2024-12-14 22:06:33 +0000

    Add extraction test.

--- tests/AccessSalaryImporter/AccessSalaryImporterTest.py
+++ tests/AccessSalaryImporter/AccessSalaryImporterTest.py
@@ -2,6 +2,8 @@ import datetime
 import unittest
 from beancountimporters.AccessSalaryImporter.AccessSalaryImporter import Importer, pdf_to_text
 from beancount.ingest import cache
+from beancount.core.amount import Amount
+from beancount.core.number import D
 
 
 class AccessSalaryTestCase(unittest.TestCase):
@@ -41,6 +43,47 @@ class AccessSalaryTestCase(unittest.TestCase):
     def test_FileDate(self):
         date = self.importer.file_date(self.salaryFile)
         self.assertEqual(date, datetime.date(2024, 10, 28))
+
+    def test_ExtractionReturnsCorrectValues(self):
+        transactions = self.importer.extract(self.salaryFile)
+        self.assertEqual(len(transactions), 1)
+        transaction = transactions[0]
+        self.assertEqual(transaction.date, datetime.date(2024, 10, 28))
+        self.assertEqual(transaction.payee, "SELF")
+        self.assertEqual(len(transaction.postings), 7)
+        self.assertEqual(transaction.tags, frozenset())
+        self.assertEqual(transaction.links, frozenset())
+
+        postings = transaction.postings
+
+        netPay = postings[0]
+        self.assertEqual(netPay.account, "CurrentAccount")
+        self.assertEqual(netPay.units, Amount(D("2112.33"), "GBP"))
+
+        studentLoan = postings[1]
+        self.assertEqual(studentLoan.account, "StudentLoanAccount")
+        self.assertEqual(studentLoan.units, Amount(D("28.00"), "GBP"))
+
+        payTax = postings[2]
+        self.assertEqual(payTax.account, "PAYE")
+        self.assertEqual(payTax.units, Amount(D("328.60"), "GBP"))
+
+        natInsurance = postings[3]
+        self.assertEqual(natInsurance.account, "NationalInsuranceAccount")
+        self.assertEqual(natInsurance.units, Amount(D("123.56"), "GBP"))
+
+        pensionAsset = postings[4]
+        self.assertEqual(pensionAsset.account, "PensionAssetAccount")
+        self.assertEqual(pensionAsset.units, Amount(D("283.34"), "GBP"))
+
+        pensionMatch = postings[5]
+        self.assertEqual(pensionMatch.account, "PensionMatchAccount")
+        self.assertEqual(pensionMatch.units, Amount(-D("141.67"), "GBP"))
+
+        grossPay = postings[6]
+        self.assertEqual(grossPay.account, "SalaryAccount")
+        self.assertEqual(grossPay.units, Amount(-D("2,833.33"), "GBP"))
+
 
 
 if __name__ == '__main__':
