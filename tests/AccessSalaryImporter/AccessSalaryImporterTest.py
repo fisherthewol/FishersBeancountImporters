05faa3aefb8c07054d41882f938c4c36c22e8b42
George Kokinis <george.kokinis@gmail.com>
2024-12-14 21:02:22 +0000

    Add all tests bar extract test.

--- tests/AccessSalaryImporter/AccessSalaryImporterTest.py
+++ tests/AccessSalaryImporter/AccessSalaryImporterTest.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 from beancountimporters.AccessSalaryImporter.AccessSalaryImporter import Importer, pdf_to_text
 from beancount.ingest import cache
@@ -17,16 +18,29 @@ class AccessSalaryTestCase(unittest.TestCase):
             y2kfix="20",
             flag="!")
         self.salaryFile = cache._FileMemo("../TestFiles/2024-10-25 My Payslip 28-OCT-24.pdf")
+        self.amexFile = cache._FileMemo("../TestFiles/Amex.csv")
         with open("../TestFiles/SalaryText.txt", 'r', encoding="unicode_escape") as file:
             self.salaryText = file.read()
+
+    def test_pdfToTextCorrectlyExtracts(self):
+        pdfText = pdf_to_text(self.salaryFile.name)
+        self.assertEqual(pdfText, self.salaryText)
 
     def test_ImporterCorrectlyIdentifiesPaychequePDF(self):
         salaryIndentify = self.importer.identify(self.salaryFile)
         self.assertEqual(salaryIndentify, True)
 
-    def test_pdfToTextCorrectlyExtracts (self):
-        pdfText = pdf_to_text(self.salaryFile.name)
-        self.assertEqual(pdfText, self.salaryText)
+    def test_ImporterCorrectlyDeniesAmexCSV(self):
+        salaryIndentify = self.importer.identify(self.amexFile)
+        self.assertEqual(salaryIndentify, False)
+
+    def test_FileAccount(self):
+        account = self.importer.file_account(self.salaryFile)
+        self.assertEqual(account, "SalaryAccount")
+
+    def test_FileDate(self):
+        date = self.importer.file_date(self.salaryFile)
+        self.assertEqual(date, datetime.date(2024, 10, 28))
 
 
 if __name__ == '__main__':
