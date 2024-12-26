187be6f6ea281936f7a4c5e073ca4be907278ce4
George Kokinis <george.kokinis@gmail.com>
2024-12-26 03:39:59 +0000

    All tests for member functions on QifImporter. Fix an possible unknown exception in GetQifAccount.

--- tests/test_QifImporter.py
+++ tests/test_QifImporter.py
@@ -7,13 +7,19 @@ from beancount.core.number import D
 from beancountimporters.QifImporter.QifImporter import QifImporter
 from tests.Utilities import GetTestFilesDir
 
+from quiffen import Qif
+
+
 class QifTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.importer = QifImporter(destinationaccount="DestinationAccount", dayfirst=True, )
+        self.importer = QifImporter(destinationaccount="DestinationAccount", dayfirst=True)
         testFilesDir = GetTestFilesDir()
         self.salaryFile = cache._FileMemo((testFilesDir / "2024-10-25 My Payslip 28-OCT-24.pdf").absolute().as_posix())
         self.lloydsCcFile = cache._FileMemo((testFilesDir / "LloydsCC.qif").absolute().as_posix())
+
+    def test_Name(self):
+        self.assertEqual(self.importer.name(), "QifImporter.DestinationAccount")
 
     def test_ImporterCorrectlyIdentifiesLloydsQIF(self):
         lloydsIdentify = self.importer.identify(self.lloydsCcFile)
@@ -46,6 +52,28 @@ class QifTestCase(unittest.TestCase):
         posting = testTxn.postings[0]
         self.assertEqual(posting.account, "DestinationAccount")
         self.assertEqual(posting.units, Amount(-D("4.70"), "GBP"))
+
+    def test_GetQifAccountThrowsErrorWhenQifObjectDoesntYetExist(self):
+        with self.assertRaises(ValueError) as cm:
+            self.importer.GetQifAccount()
+
+        exception = cm.exception
+        self.assertEqual(str(exception), "QifObject is None so an account cannot be determined.")
+
+    def test_GetQifDAccountReturnsDefaultAccount(self):
+        self.importer.qifObject = Qif.parse(self.lloydsCcFile.name, day_first=True)
+        qifAccount = self.importer.GetQifAccount()
+        self.assertEqual(qifAccount.name, "Quiffen Default Account")
+        self.importer.qifObject = None
+        self.importer.qifAccount = ""
+
+    def test_GetQifAccountReturnsNoneForBadAccount(self):
+        self.importer.qifObject = Qif.parse(self.lloydsCcFile.name, day_first=True)
+        self.importer.qifAccount = "BadAccount"
+        qifAccount = self.importer.GetQifAccount()
+        self.assertEqual(qifAccount, None)
+        self.importer.qifObject = None
+        self.importer.qifAccount = ""
 
 
 if __name__ == '__main__':
